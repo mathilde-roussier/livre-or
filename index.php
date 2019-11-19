25147621518223eb2d695211fe3ffff2ab1d4847
Mathilde <mathilde.roussier@laplateforme.io>
2019-11-19 15:24:56 +0100

    Mise en place du css et code commun

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php session_start(); ?>
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Site de Mathilde</title>
+    <link rel="stylesheet" href="css/livre-or.css">
+</head>
+
+<body class= "body_base">
+
+    <header class="hetf">
+        <nav>
+            <ul>
+                <li><a href="index.php"><img src='img/logo-accueil.png'></a></li>
+
+                <?php if (!isset($_SESSION['login'])) : ?>
+
+                    <li><a href="inscription.php">Inscription</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li><a href="connexion.php">Connexion</a></li>
+
+                <?php else : ?>
+
+                    <li><a href="commentaire.php">Commentaire</a></li>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li>
+                        <form action="index.php" method="post">
+                            <input type="submit" name='deco' value="Deconnexion">
+                        </form>
+                        <?php if (isset($_POST['deco'])) {
+                                session_unset();
+                                session_destroy();
+                                header('Location:index.php');
+                            }
+                            ?>
+                    </li>
+
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+        <section>
+
+            <?php if (isset($_SESSION['login'])) : ?>
+                <h1> Bienvenue <?php echo $_SESSION['login'] ?>,</h1>
+                <p>vous vous trouvez sur le livre d'or de Mathilde !</p>
+            <?php else : ?>
+                <h1> Bienvenue, </h1>
+                <p>Vous vous trouvez sur le livre d'or de Mathilde !</p>
+            <?php endif; ?>
+
+        </section>
+
+    </main>
+
+    <footer class="hetf">
+        <nav>
+            <ul>
+
+                <?php if (!isset($_SESSION['login'])) : ?>
+
+                    <li><a href="inscription.php">Inscription</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li><a href="connexion.php">Connexion</a></li>
+
+                <?php else : ?>
+
+                    <li><a href="commentaire.php">Commentaire</a></li>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+        
+                <?php endif; ?>
+
+            </ul>
+
+            <aside>
+                <p>Copyright 2019 Coding School | All Rights Reserved | Project by Mathilde.</p>
+            </aside>
+
+        </nav>
+    </footer>
+
+</body>
+
+</html>
---
+++ livre-or.php
@@ -0,0 +1,86 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Livre d'or</title>
+    <link rel="stylesheet" href="css/livre-or.css">
+</head>
+
+<body class="body_livre">
+
+    <header class="hetf">
+        <nav>
+            <ul>
+                <li><a href="index.php"><img src='img/logo-accueil.png'></a></li>
+
+                <?php if (!isset($_SESSION['login'])) : ?>
+
+                    <li><a href="inscription.php">Inscription</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li><a href="connexion.php">Connexion</a></li>
+
+                <?php else : ?>
+
+                    <li><a href="commentaire.php">Commentaire</a></li>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li>
+                        <form action="index.php" method="post">
+                            <input type="submit" name='deco' value="Deconnexion">
+                        </form>
+                        <?php if (isset($_POST['deco'])) {
+                                session_unset();
+                                session_destroy();
+                                header('Location:index.php');
+                            }
+                            ?>
+                    </li>
+
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
+
+
+    <footer class="hetf">
+        <nav>
+            <ul>
+
+                <?php if (!isset($_SESSION['login'])) : ?>
+
+                    <li><a href="inscription.php">Inscription</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li><a href="connexion.php">Connexion</a></li>
+
+                <?php else : ?>
+
+                    <li><a href="commentaire.php">Commentaire</a></li>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="livre-or.php">Livre d'or</a></li>
+                    <li>
+                        <form action="index.php" method="post">
+                            <input type="submit" name='deco' value="Deconnexion">
+                        </form>
+                        <?php if (isset($_POST['deco'])) {
+                                session_unset();
+                                session_destroy();
+                                header('Location:index.php');
+                            }
+                            ?>
+                    </li>
+
+                <?php endif; ?>
+            </ul>
+
+            <aside>
+                <p>Copyright 2019 Coding School | All Rights Reserved | Project by Mathilde.</p>
+            </aside>
+
+        </nav>
+    </footer>
+
+</body>
+
+</html>
